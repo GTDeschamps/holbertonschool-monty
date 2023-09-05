931ccd966de94afe6d43d68f19fcf8c8d21bdc7c
GTDeschamps <g.deschamps@hotmail.fr>
2023-09-05 09:33:23 +0200

    betty style ( but main need more optimisation)

--- main.c
+++ main.c
@@ -18,14 +18,8 @@ int main(int argc, char *argv[])
 	unsigned int line_number = 0;
 	stack_t *stack = NULL;
 	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL}};
+		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
+		{"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}};
 
 	if (argc != 2)
 	{
@@ -49,6 +43,7 @@ int main(int argc, char *argv[])
 			continue;
 
 		int found = 0;
+
 		for (int i = 0; instructions[i].opcode != NULL; i++)
 		{
 			if (strcmp(opcode, instructions[i].opcode) == 0)
@@ -58,7 +53,6 @@ int main(int argc, char *argv[])
 				break;
 			}
 		}
-
 		if (!found)
 		{
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
@@ -68,7 +62,6 @@ int main(int argc, char *argv[])
 			exit(EXIT_FAILURE);
 		}
 	}
-
 	free(line);
 	fclose(file);
 	free_stack(&stack);
