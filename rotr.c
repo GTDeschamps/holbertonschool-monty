fa45d782a1e46a9754fa313b6b279e102ca07230
shane <6874@holbertonstudents.com>
2023-09-06 16:41:25 +0200

    correction rotl() + ajout rotr()

--- rotr.c
+++ rotr.c
@@ -1,31 +1,28 @@
 #include "monty.h"
 
 /**
- * rotl -computes the rest of the division of the second top element.
+ * rotr -computes the rest of the division of the second top element.
  * @line_number: Line number in the bytecode file.
  * @stack: Double pointer to the top of the stack.
  */
 
-void rotl(stack_t **stack, unsigned int line_number)
+void rotr(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
 	stack_t *ptr;
 	stack_t *ptr2;
 
 	ptr = *stack;
-	ptr2 = (*stack)->next;
 
 	if (*stack != NULL || (*stack)->next != NULL)
 	{
 		while (ptr->next != NULL)
 			ptr = ptr->next;
-
-		ptr2->prev = NULL;
-
+		ptr2 = ptr->prev;
+		ptr2->next = NULL;
+		ptr->prev = NULL;
 		ptr->next = *stack;
 		(*stack)->prev = ptr;
-
-		(*stack)->next = NULL;
-		*stack = ptr2;
+		*stack = ptr;
 	}
 }
