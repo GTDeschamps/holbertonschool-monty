827d5c7b38e7cb7aae1ee22e459dbc6394683f1b
shane <6874@holbertonstudents.com>
2023-09-06 09:33:54 +0200

    ajout de la task 6 (SUB)

--- monty.h
+++ monty.h
@@ -49,5 +49,6 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void read_instructions(FILE *file, instruction_t *instructions);
+void sub(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
---
+++ sub.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * sub - Subs the top two elements of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the bytecode file.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int result;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	result = (*stack)->next->n - (*stack)->n;
+	(*stack)->next->n = result;
+	pop(stack, line_number);
+}
